cc649e2daf28b9672ebc03bc1790c856c1b2747e
AlonsoM207 <alonsoabimaelmoralesreyna@gmail.com>
2024-01-26 14:02:34 -0600

    Camara funcional y videos

--- prueba/ScanVideoView.swift
+++ prueba/ScanVideoView.swift
@@ -7,58 +7,36 @@
 
 import SwiftUI
 import YouTubePlayerKit
+import AVKit
 
 struct ScanVideoView: View {
-    var ytLink: String
+    //var ytLink: String
     var content: String
-    var vid: String
-    let configuration = YouTubePlayer.Configuration(
-        // Define which fullscreen mode should be used (system or web)
-        fullscreenMode: .system,
-        // Enable auto play
-        autoPlay: true,
-        // Hide controls
-        showControls: true,
-        showFullscreenButton: true,
-        loopEnabled: true,
-        useModestBranding: true,
-        showRelatedVideos: false
-        
-    )
     
     var body: some View {
-        
-            ZStack{
-                Color(.background).ignoresSafeArea()
-                VStack{
-                    /*RoundedRectangle(cornerRadius: 10)
-                     .frame(width: 350.0, height: 600.0)*/
-                    Text(content).font(Font.varTitle).foregroundColor(.white)
-                    YouTubePlayerView(
-                        YouTubePlayer(
-                            source: .url(vid),
-                            configuration: configuration
-                        )
-                    ).frame(height: 560)
-                        .cornerRadius(10.0)
-                        .padding([.leading, .trailing])
-                    /*NavigationLink(destination: GlosarioABCView()){
-                        Capsule().fill(Color.accentColor).overlay(
-                            HStack(alignment: .center){
-                                Image("Arrow")
-                                Text("regresar").foregroundColor(.secundarycolor).font(Font.varButtonLabel)
-                            }
-                            
-                        ).frame(width: 175.0, height: 50.0)
-                    }*/
+        ZStack {
+            Color(.background).edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
                     
-                    
+                    Text(content)
+                        .font(Font.varTitle)
+                        .foregroundColor(.white)
+                        .lineLimit(2)
                 }
+                .padding()
+                VideoPlayer(player:  AVPlayer(url: Bundle.main.url(forResource: content,
+                                                                   withExtension: "mp4")!))
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .cornerRadius(15.0)
+                .padding([.leading, .trailing])
             }
-        
+        }
     }
 }
 
-#Preview {
-    ScanVideoView(ytLink: "URL", content: "Aa", vid: "https://youtu.be/32GZ3suxRn4")
+struct ScanVideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScanVideoView(content: "1")
+    }
 }
---
+++ prueba/ScanVideoView2.swift
@@ -0,0 +1,64 @@
+//
+//  SignVideoView.swift
+//  prueba
+//
+//  Created by Alumno on 14/01/24.
+//
+
+import SwiftUI
+import YouTubePlayerKit
+
+struct ScanVideoView2: View {
+    var ytLink: String
+    var content: String
+    var vid: String
+    let configuration = YouTubePlayer.Configuration(
+        // Define which fullscreen mode should be used (system or web)
+        fullscreenMode: .system,
+        // Enable auto play
+        autoPlay: true,
+        // Hide controls
+        showControls: true,
+        showFullscreenButton: true,
+        loopEnabled: true,
+        useModestBranding: true,
+        showRelatedVideos: false
+        
+    )
+    
+    var body: some View {
+        
+            ZStack{
+                Color(.background).ignoresSafeArea()
+                VStack{
+                    /*RoundedRectangle(cornerRadius: 10)
+                     .frame(width: 350.0, height: 600.0)*/
+                    Text(content).font(Font.varTitle).foregroundColor(.white)
+                    YouTubePlayerView(
+                        YouTubePlayer(
+                            source: .url(vid),
+                            configuration: configuration
+                        )
+                    ).frame(height: 560)
+                        .cornerRadius(10.0)
+                        .padding([.leading, .trailing])
+                    /*NavigationLink(destination: GlosarioABCView()){
+                        Capsule().fill(Color.accentColor).overlay(
+                            HStack(alignment: .center){
+                                Image("Arrow")
+                                Text("regresar").foregroundColor(.secundarycolor).font(Font.varButtonLabel)
+                            }
+                            
+                        ).frame(width: 175.0, height: 50.0)
+                    }*/
+                    
+                    
+                }
+            }
+        
+    }
+}
+
+#Preview {
+    ScanVideoView2(ytLink: "URL", content: "Aa", vid: "https://youtu.be/32GZ3suxRn4")
+}
